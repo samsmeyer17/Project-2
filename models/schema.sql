9804fe14a4663f6c5a42530ade85c8ecb94fb67f
Samuel Meyer <sam.smeyer17@gmail.com>
2019-10-03 19:30:55 -0700

    adding to schema.sql to create a users table to contain the user data upon sign up

--- models/schema.sql
+++ models/schema.sql
@@ -1,2 +1,10 @@
 DROP DATABASE IF EXISTS workout_trackerDB;
-CREATE DATABASE workout_trackerDB
+CREATE DATABASE workout_trackerDB;
+USE workout_trackerDB;
+
+CREATE TABLE Users (
+  name VARCHAR(300) NOT NULL PRIMARY KEY,
+  email VARCHAR(300) NOT NULL,
+  password VARCHAR(300) NOT NULL,
+  
+),
