ae1d5f4faa40af786a73c0afebd94db9ed625ebb
Samuel Meyer <sam.smeyer17@gmail.com>
2019-10-07 20:28:11 -0700

    starting over with signup

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   // Getting references to our form and inputs
   var loginForm = $("form.login");
-  var nameInput = $("input#name-input");
-  var ageInput = $("input#age-input")
+  // var nameInput = $("input#name-input");
+  // var ageInput = $("input#age-input")
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
 
@@ -10,8 +10,8 @@ $(document).ready(function() {
   loginForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
-      name: nameInput.val().trim(),
-      age: ageInput.val().trim(),
+      // name: nameInput.val().trim(),
+      // age: ageInput.val().trim(),
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
@@ -21,18 +21,18 @@ $(document).ready(function() {
     }
 
     // If we have an email and password we run the loginUser function and clear the form
-    loginUser(userData.name, userData.age, userData.email, userData.password);
-    nameInput.val("");
-    ageInput.val("");
+    loginUser(userData.email, userData.password);
+    // nameInput.val("");
+    // ageInput.val("");
     emailInput.val("");
     passwordInput.val("");
   });
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-  function loginUser(name, age, email, password) {
+  function loginUser(email, password) {
     $.post("/api/login", {
-      name: name,
-      age: age,
+      // name: name,
+      // age: age,
       email: email,
       password: password
     })
